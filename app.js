02eab606c8ccbfb72282e4a3aa7da0e1b446b3e1
s-moe <sarahannmoe@gmail.com>
2021-10-26 11:15:07 -0700

    attack Monster method created

---
+++ app.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+    };
+  },
+  methods: {
+    attackMonster() {
+      const attackValue = Math.floor(Math.random() * (12 - 5)) + 5;
+      this.monsterHealth -= attackValue;
+    },
+  },
+});
+app.mount("#game");
